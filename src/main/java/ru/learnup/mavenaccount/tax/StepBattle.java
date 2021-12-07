88340a0513bdfee414ae44ccb7c3c0d641c1afe2
rovnanov <arpacc@yandex.ru>
2021-12-07 15:46:15 +0300

    Addeed StepBattle class

---
+++ src/main/java/ru/learnup/mavenaccount/tax/StepBattle.java
@@ -0,0 +1,41 @@
+package ru.learnup.mavenaccount.tax;
+import java.util.HashMap;
+public class StepBattle {
+    private HashMap<Integer, Integer> stepCounter = new HashMap<>();
+    private HashMap<Integer, Integer> stepCounter2 = new HashMap<>();
+    private int player;
+
+    public void addSteps(int player, int day, int steps) {
+        if (player == 1) {
+            if (stepCounter.containsKey(day)) {
+                stepCounter.put(day, stepCounter.get(day) + steps);
+            } else {
+                stepCounter.put(day, steps);
+            }
+        } else {
+            if (stepCounter2.containsKey(day)) {
+                stepCounter2.put(day, stepCounter2.get(day) + steps);
+            } else {
+                stepCounter2.put(day, steps);
+            }
+        }
+    }
+
+    public int winner() {
+        int tmpSteps = 0;
+        int tmpSteps2 = 0;
+        int result;
+        for (int day : stepCounter.keySet()) {
+            tmpSteps += stepCounter.get(day);
+        }
+        for (int day : stepCounter2.keySet()) {
+            tmpSteps2 += stepCounter2.get(day);
+        }
+        if (tmpSteps > tmpSteps2) {
+            result = 1;
+        } else {
+            result = 2;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/BattleTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.learnup.mavenaccount.tax.StepBattle;
+
+public class BattleTest {
+    @Test
+    public void battleTest(){
+        StepBattle battle = new StepBattle();
+        battle.addSteps(1,1,10);
+        battle.addSteps(1,2,10);
+        battle.addSteps(1,3,10);
+        battle.addSteps(2,1,10);
+        battle.addSteps(2,2,10);
+        battle.addSteps(2,3,15);
+
+        int expected = 2;
+        int actual = battle.winner();
+
+        Assertions.assertEquals(expected,actual);
+    }
+}
